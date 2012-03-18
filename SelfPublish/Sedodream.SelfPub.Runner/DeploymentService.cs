3fff2ee908da7a899ca125137a090265e95efed2
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-03-18 12:50:12 -0700

    Adding service project

---
+++ SelfPublish/Sedodream.SelfPub.Runner/DeploymentService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Sedodream.SelfPub.Runner {
+    public partial class DeploymentService : ServiceBase {
+        public DeploymentService() {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args) {
+        }
+
+        protected override void OnStop() {
+        }
+    }
+}
