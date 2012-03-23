4525610159f9f998a8a8aa74940e1b748f9cce6c
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-03-22 20:34:53 -0700

    Adding SampleWeb and updating to publish to blob storage on publish

---
+++ SelfPublish/Samples/SampleWeb/BundleConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SampleWeb {
+    public class BundleConfig {
+        public static void RegisterTemplateBundles() {
+            Bundle jsBundle = new Bundle("~/bundles/WebFormsJs", new JsMinify());
+            jsBundle.AddFile("~/Scripts/WebForms/WebForms.js");
+            jsBundle.AddFile("~/Scripts/WebForms/MenuStandards.js");
+            jsBundle.AddFile("~/Scripts/WebForms/Focus.js");
+            jsBundle.AddFile("~/Scripts/WebForms/GridView.js");
+            jsBundle.AddFile("~/Scripts/WebForms/DetailsView.js");
+            jsBundle.AddFile("~/Scripts/WebForms/TreeView.js");
+            jsBundle.AddFile("~/Scripts/WebForms/WebParts.js");
+            BundleTable.Bundles.Add(jsBundle);
+
+            // Order is very important for these files to work, they have explicit dependencies
+            Bundle ajaxBundle = new Bundle("~/bundles/MsAjaxJs", new JsMinify());
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjax.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxCore.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxSerialization.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxNetwork.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebServices.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxComponentModel.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxGlobalization.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxHistory.js");
+            ajaxBundle.AddFile("~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js");
+            BundleTable.Bundles.Add(ajaxBundle);
+
+            Bundle cssBundle = new Bundle("~/Content/css", new CssMinify());
+            cssBundle.TryAddFile("~/Content/site.css");
+            BundleTable.Bundles.Add(cssBundle);
+
+            Bundle jqueryUiCssBundle = new Bundle("~/Content/themes/base/css", new CssMinify());
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.core.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.resizable.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.selectable.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.accordion.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.autocomplete.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.button.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.dialog.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.slider.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.tabs.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.datepicker.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.progressbar.css");
+            jqueryUiCssBundle.TryAddFile("~/Content/themes/base/jquery.ui.theme.css");
+            BundleTable.Bundles.Add(jqueryUiCssBundle);
+
+        }
+    }
+}
---
+++ SelfPublish/Samples/SampleWeb/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SampleWeb._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h2>Sample site for publishing</h2>
+            </hgroup>
+<%--            <p>
+                To learn more about ASP.NET, visit <a href="http://asp.net" title="ASP.NET Website">http://asp.net</a>. The page features <mark>videos, tutorials, and samples</mark>to help you get the most from ASP.NET. If you have any questions about ASP.NET visit <a href="http://forums.asp.net/18.aspx" title="ASP.NET Forum">our forums</a>.
+            </p>--%>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+</asp:Content>
