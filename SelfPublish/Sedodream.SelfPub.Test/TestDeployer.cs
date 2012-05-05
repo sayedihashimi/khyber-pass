51f0540209596926dde02617fc620866ec3882f3
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2012-05-05 14:53:07 -0700

    Updating failing unit test

--- SelfPublish/Sedodream.SelfPub.Test/TestDeployer.cs
+++ SelfPublish/Sedodream.SelfPub.Test/TestDeployer.cs
@@ -1,17 +1,72 @@
 ﻿namespace Sedodream.SelfPub.Test {
     using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Sedodream.SelfPub.Common;
-using Sedodream.SelfPub.Common.Deploy;
-using Sedodream.SelfPub.Common.Exceptions;
-using Sedodream.SelfPub.Test.Helpers;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using log4net.Config;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MongoDB.Driver;
+    using Sedodream.SelfPub.Common;
+    using Sedodream.SelfPub.Common.Deploy;
+    using Sedodream.SelfPub.Common.Exceptions;
+    using Sedodream.SelfPub.Test.Helpers;
 
     [TestClass]
     public class TestDeployer {
+        private static Process mongoDbProcess;
+
+        [ClassInitialize]
+        public static void Initalize(TestContext testContext) {
+            XmlConfigurator.Configure();
+            // we have to start mongo DB
+            DirectoryInfo mongoDbDir = GetMongoDbDir(testContext);
+            var mongodbexe = mongoDbDir.GetFiles("mongod.exe").Single();
+
+            DirectoryInfo testCtxDirectory = new DirectoryInfo(testContext.TestDir);
+            DirectoryInfo dataDbDir = testCtxDirectory.CreateSubdirectory(new Config().GetAppSetting<string>(CommonTestStrings.MongodbDir));
+
+            string connectionString = new Config().GetConnectionString(CommonStrings.Database.ConnectionStringName).ConnectionString;
+            MongoUrlBuilder mub = new MongoUrlBuilder(connectionString);
+            string args = string.Format(@"--dbpath ""{0}"" --port {1}", dataDbDir.FullName, mub.Server.Port);
+
+            var psi = new ProcessStartInfo {
+                FileName = mongodbexe.FullName,
+                WorkingDirectory = testCtxDirectory.FullName,
+                Arguments = args,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            mongoDbProcess = Process.Start(psi);
+        }
+
+        [ClassCleanup]
+        public static void Cleanup() {
+            // we have to stop mongo DB
+            mongoDbProcess.CloseMainWindow();
+            mongoDbProcess.WaitForExit(5 * 1000);
+            if (!mongoDbProcess.HasExited) {
+                mongoDbProcess.Kill();
+            }
+        }
+
+        private static DirectoryInfo GetMongoDbDir(TestContext testContext) {
+            if (testContext == null) { throw new ArgumentNullException("testContext"); }
+
+            var mongoDbBindir =
+                new DirectoryInfo(testContext.TestDir)
+                    .Parent.Parent.Parent
+                    .GetDirectories("lib").Single()
+                    .GetDirectories("mongodb*").Single()
+                    .GetDirectories("bin").Single();
+
+            return mongoDbBindir;
+        }
+
         [TestMethod]
         public void Test_GetHandler_MSDeploy() {
             Deployer deployer = new Deployer();
